572e845cf59f346634a8b500f844eaee5377df37
user126179663 <115039826+user126179663@users.noreply.github.com>
2022-10-18 15:06:50 +0900

    Add files via upload

--- background.js
+++ background.js
@@ -64,24 +64,37 @@ class OpenAsUrl extends WX {
 		
 	}
 	
-	async init() {
+	init() {
 		
-		this.setting = {};
-		
-		const { DEFAULT_SETTINGS_PATH } = OpenAsUrl,
-				{ browser, setting } = this,
-				{ contextMenus, storage } = this.browser;
-		let k, saves, stored, defaultSetting;
-		
-		await fetch(DEFAULT_SETTINGS_PATH).then(fetched => fetched.json()).then(json => defaultSetting = json),
-		
-		await this.getStorage().then(storage => stored = storage);
-		
-		for (k in defaultSetting) setting[k] = k in stored ? stored[k] : (saves ||= true, defaultSetting[k]);
-		
-		this.updateMenu?.(setting["main-item-in-context-menu"].option, true),
-		
-		saves && await this.setStorage(setting);
+		return this.inizitalized || !this.available ?
+			(
+				this.initialized = false,
+				this.available = new Promise(async rs => {
+					
+					this.setting = {};
+					
+					const { DEFAULT_SETTINGS_PATH } = OpenAsUrl,
+							{ browser, setting } = this,
+							{ contextMenus, storage } = this.browser;
+					let k, saves, stored, defaultSetting;
+					
+					await fetch(DEFAULT_SETTINGS_PATH).then(fetched => fetched.json()).then(json => defaultSetting = json),
+					
+					await this.getStorage().then(storage => stored = storage);
+					
+					for (k in defaultSetting) setting[k] = k in stored ? stored[k] : (saves ||= true, defaultSetting[k]);
+					
+					this.updateMenu?.(setting["main-item-in-context-menu"].option, true),
+					
+					saves && await this.setStorage(setting);
+					
+					this.initialized = true,
+					
+					rs(this);
+					
+				})
+			) :
+			this.available;
 		
 	}
 	
@@ -127,7 +140,7 @@ class OpenAsUrl extends WX {
 			
 		} else {
 			
-			setting['enabled-notifications'] && notifications?.create?.(this.notification.noUrl);
+			setting['enabled-notifications'] && (notifications?.create?.(this.notification.noUrl));
 			
 		}
 		
@@ -146,26 +159,44 @@ class OpenAsUrl extends WX {
 	
 }
 
+// オブジェクトが HTMLElement を継承する場合、インスタンスを作る前にオブジェクトを CustomElementRegistry.define で要素として定義する必要がある。
+// でなければ Illigal constructor. のエラーになる。
 customElements.define(OpenAsUrl.tagName, OpenAsUrl);
 
-const openAsUrl = new OpenAsUrl(browser || chrome), { browser: wx } = openAsUrl;
+// オブジェクト OpenAsUrl は、初期化処理内に非同期処理が含まれている。
+// これは background が実行中の時は問題になりにくいが、
+// background は一度停止したあとにイベントなどを通じて再び実行されると、起動時と同じ処理を再び繰り返す。
+// これは実質的な再起動で、停止前の変数の値などはすべて失われており、クロージャなどを通じて停止前と後での直接的な値の受け渡しもできない。
+// こうした仕様そのものは実際にはあまり問題にならないかもしれないが、
+// 以下のようにインスタンスを作成する時に、コンストラクター内に先に述べたような非同期処理が含まれていた場合、
+// その非同期処理の完了前にイベントリスナーの実行が行なわれ、かつリスナーの中にインスタンスを使用する処理が含まれていると、
+// インスタンスのプロパティの作成が間に合わなかった場合に処理に不整合を引き起こす恐れが生じる。
+// そのため、ここではインスタンスの初期化処理の完了で解決する Promise を示すプロパティをインスタンスに作成し、
+// イベントリスナー内ではそのプロパティが持つ Promise の解決後に続く処理を実行するようにしている。
+// 具体的には openAsUrl.available が初期化処理の完了を確認できるプロパティで、
+// openAsUrl.available.then(...) で、イベントが通知されたリスナー内ののインスタンスを使う処理を実行している。
+const openAsUrl = new OpenAsUrl(browser || chrome);
 
-wx.storage.onChanged?.addListener?.(storageChange => {
+browser.action.onClicked.addListener(() => browser.runtime.openOptionsPage()),
+
+browser.storage?.onChanged?.addListener?.(storageChange => {
 		
 		const setting = WX.getCurrentStorage(storageChange);
 		
-		openAsUrl.update(setting);
+		openAsUrl.available.then(() => openAsUrl.update(setting));
 		
 	}),
-wx.contextMenus.onClicked?.addListener?.((info, tab) => {
+browser.contextMenus?.onClicked?.addListener?.((info, tab) => {
 		
-		openAsUrl.openUrls(
-				OpenAsUrl.getUrls(
-						info.selectionText,
-						openAsUrl.setting['upgrade-https'],
-						openAsUrl.setting['default-protocol']
-					),
-				tab.index + 1
+		openAsUrl.available.then(() =>
+				openAsUrl.openUrls(
+						OpenAsUrl.getUrls(
+								info.selectionText,
+								openAsUrl.setting['upgrade-https'],
+								openAsUrl.setting['default-protocol']
+							),
+						tab.index + 1
+					)
 			);
 		
 	});
